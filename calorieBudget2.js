9210064843fd80dccaafb3ba6b794a475501c5ad
wylted <wylted82@gmail.com>
2020-04-12 21:11:21 -0400

    reworking the html to make javascript easier/ will replace original files

--- calorieBudget2.js
+++ calorieBudget2.js
@@ -1,32 +1,23 @@
 
-
 function bmr() {
-	var kilograms = getElementById('kilograms').value;
-    var pounds = getElementById('pounds').value;
-
-var w = parseInt(kilograms) + parseInt(pounds)/2.025;	
- 
- 
- var centimeters = getElementById('centimeters').value;
- var feet = getElementById('feet').value;
- var inches = getElementById('inches').value;
-
- var h = parseInt(centimeters) + parseInt(feet) * 30.48 + parseInt(inches) * 2.54; 
-
-
-var calculate;
-var a = getElementById('age').value;
+	
 var sex = getElementById('sex').value;
+var w_unit = getElementById('w_unit').value;
+var w = getElementById('weight').value;
+var h = getElementById('height').value; 
+var a = getElementById('age').value;
+var activity = getElementById('activity').value;
+var calculate;
 
 
-if (sex > 1) {
+
+if (sex = female) {
      calculate = 447.593 + 9.247 * parseInt(w) + 3.098 * parseInt(h) - 4.330 * parseInt(a);
 } else {
 	 calculate = 88.362 + 13.397 * parseInt(w) + 4.799 * parseInt(h) - 5.677 * parseInt(a);
 
 }
-var activity = getElementById('activity').value;
-
+//document.getElementById("demo").innerHTML = calculate * activity;
 
 document.write(Math.trunc(calculate * activity)); 
 }
