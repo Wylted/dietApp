b6db99df6bc1cc70454c6526891070717c671ec9
wylted <wylted82@gmail.com>
2020-04-15 15:31:35 -0400

    can calculate macros and calorie budget

--- calorieBudget.js
+++ calorieBudget.js
@@ -23,12 +23,15 @@ function bmr() {
 	}
 
     var bmr = Math.trunc(calculate * activity);
- 
+    var goal = document.getElementById('goal').value;
+    var budget = Math.trunc(goal * bmr);
+    var protein = Math.trunc(budget * 0.30 / 4);
+    var carbs = Math.trunc(budget * 0.45 / 4);
+    var fat = Math.trunc(budget * 0.25 / 9); 
 
 
-	document.getElementById('bmr').innerHTML ="Your BMR is <span id='tdee'>" + bmr + " </span>calories";	
-
-
+	document.getElementById('bmr').innerHTML ="Your TDEE is <span id='tdee'>" + bmr + " </span>calories";	
+    document.getElementById('calorie-budget').innerHTML =  "Your Budget is " + budget + " Calories";
+    document.getElementById('macro').innerHTML = protein + " grams of protein <br>" + carbs + " grams of carbs <br>" + fat + " grams of fat";
 
 }
-
